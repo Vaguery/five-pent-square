70d7ca39cc3706eae29de077d340ed93ba15ff38
Bill Tozier <bill@vagueinnovation.com>
2021-04-02 12:49:06 -0400

    added a checks for adjacency of ominoes, contiguousness

--- fivepents.rb
+++ fivepents.rb
@@ -12,14 +12,14 @@ class Omino
     @cells = Set.new(cells)
   end
 
-  def neighbors_of_cell(cell,possible_set)
+  def self.neighbors_of_cell(cell,possible_set)
     i,j = [cell[0],cell[1]]
     orthogonal_neighbors = Set[ [i-1,j],[i+1,j],[i,j-1],[i,j+1] ]
     return possible_set & orthogonal_neighbors
   end
 
   def feasible_neighbors(possible_set)
-    @cells.inject(Set.new()) {|s,c| s | neighbors_of_cell(c,possible_set)}
+    @cells.inject(Set.new()) {|s,c| s | Omino.neighbors_of_cell(c,possible_set)}
   end
 
   def sorted_cells(cells)
@@ -49,11 +49,27 @@ class Omino
     end
     return pic
   end
+
+  def contiguous?(possible_set)
+    internal_neighbors = @cells.collect do |c|
+      @cells & Omino.neighbors_of_cell(c,possible_set) # intersection
+    end
+    return internal_neighbors.inject(true) {|answer,n| answer && !n.empty?}
+  end
+
+  def touching?(other_omino,possible_set)
+    mine = self.feasible_neighbors(possible_set)
+    !(mine & other_omino.cells).empty?
+  end
 end
 
-s = Omino.new(:A,[[4,4],[4,3],[4,2]])
-t = Omino.new(:B,[[1,1],[1,2],[0,0],[1,0]])
-puts t.neighbors_of_cell([0,0],space).inspect
-tn = t.feasible_neighbors(space)
+a = Omino.new(:A,[[4,4],[4,3],[4,2]])
+b = Omino.new(:B,[[1,1],[1,2],[0,0],[1,0]])
+c = Omino.new(:C,[[2,0],[3,0]])
+x = Omino.new(:X,[[1,4],[4,1]])
+puts Omino.neighbors_of_cell([0,0],space).inspect
+tn = b.feasible_neighbors(space)
 puts tn.to_a.sort.inspect
-puts Omino.draw_cells([t,s],space)
+puts Omino.draw_cells([a,b,c,x],space)
+
+puts x.touching?(x,space)
